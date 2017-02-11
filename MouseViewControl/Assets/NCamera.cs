144943f3df5e8666f11fdd84a56fa8b4c2a987a7
NJ <nj.namju@gmail.com>
2017-02-10 20:44:39 -0500

    update

---
+++ MouseViewControl/Assets/NCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NJSUnity;
+
+public class NCamera : MonoBehaviour {
+
+    NMouse nmouse;
+
+	// Use this for initialization
+	void Awake () {
+        nmouse = new NMouse();
+        nmouse.distanceMax = 1000f;
+        nmouse.panWeight = 0.2f;
+        nmouse.Type(NMouse.preset.Rhino);
+        nmouse.Init();
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        nmouse.UpdateLate();
+
+    }
+}
